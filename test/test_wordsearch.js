b8d2be7da66634b9c023e307124efed83211ec6c
Roderick Nuque <enuke4@gmail.com>
2021-12-02 06:08:03 +0000

    pair programming

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -4,6 +4,13 @@ const assert = chai.assert;
 const wordSearch = require('../wordsearch.js')
 
 describe("#wordSearch()", function() {
+
+  it("should return false if the the matrix is empty", function() {
+    const result = wordSearch([], 'FRANK')
+
+    assert.isFalse(result);
+  });
+
   it("should return false if the word is not present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
@@ -20,7 +27,8 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+
+  it("should return true if the word is present horizontaly", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -35,4 +43,23 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present verticaly", function() {
+    const result = wordSearch([
+      ['S', 'W', 'C', 'F', 'Q', 'U', 'F', 'L'],
+      ['E', 'E', 'I', 'N', 'F', 'E', 'R', 'D'],
+      ['I', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['N', 'M', 'J', 'T', 'E', 'V', 'N', 'G'],
+      ['F', 'H', 'C', 'S', 'Y', 'E', 'J', 'L'],
+      ['E', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['L', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['D', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ], 'SEINFELD')
+
+    assert.isTrue(result);
+  });
+
+
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,34 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    if (letters.length === 0) {
+        return false;
     }
-}
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true;
+        };
+    };
+    let verticalJoin = transpose(letters);
+    verticalJoin = verticalJoin.map(ls => ls.join(''));
+    for (l of verticalJoin) {
+        if (l.includes(word)) {
+            return true;
+        };
+    };
+    return false;
+};
 
-module.exports = wordSearch
+const transpose = (matrix) => {
+    let rows = [] // Build our columns
+    for (let i = 0; i < matrix[0].length; i++) {
+        let columns = []; // Build our rows
+        for (let j = 0; j < matrix.length; j++) {
+            columns.push(matrix[j][i]); // push matrix[j][i] into the rows
+        }
+        rows.push(columns); // push the rows into the columns
+    }
+    return rows
+};
+
+
+module.exports = wordSearch;
